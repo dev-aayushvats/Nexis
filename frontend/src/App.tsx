24465f2c98e9e38f973e8d6eaff36f89424d3211
ItsMAX0112 <aayushvats361@gmail.com>
2024-10-17 20:43:09 +0530

    started with card component, just started, dont judge yet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import ContentCard from "./components/ContentCard";
 import Navbar from "./components/Navbar";
+
+
 function App() {
   return (
     <>
       <Navbar />
+      <ContentCard title="Test title 1" line="some crazy tagline kjdsfakdfjha  dsff dsf" />
     </>
   );
 }
---
+++ frontend/src/components/ContentCard.tsx
@@ -0,0 +1,19 @@
+interface cardProps{
+    maintag?: string,
+    title?: string,
+    line?: string,
+    publishedOn?: string
+}
+
+const ContentCard = (props : cardProps) => {
+    return (
+        <div className="bg-white border-b-2 shadow-sm width flex p-4">
+            <div className="flex flex-col">
+                <h1>{props.title}</h1>
+                <h3> {props.line} </h3>
+            </div>
+        </div>
+    )
+}
+
+export default ContentCard;
