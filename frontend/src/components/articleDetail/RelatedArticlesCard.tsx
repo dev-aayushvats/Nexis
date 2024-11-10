83e78da8d0754b3cfc6b573f9b958b89687791f7
Satyam Thakur <sat.thakur20@gmail.com>
2024-11-10 12:58:56 +0530

    Modifying RelatedArticlesCard

--- frontend/src/components/articleDetail/RelatedArticlesCard.tsx
+++ frontend/src/components/articleDetail/RelatedArticlesCard.tsx
@@ -13,16 +13,23 @@ const RelatedArticlesCard: React.FC<IRelatedArticlesCard> = (props) => {
   const { topic, title, publishedDate, imageUrl, readingTime } = props;
 
   return (
-    <div className="card bg-base-100 shadow-xl">
-      <figure>
-        <img src={imageUrl} alt={title} />
-      </figure>
-      <div className="p-4">
-        <TopicChips topic={topic} />
-        <h2 className="card-title mt-2">{title}</h2>
-        <div className="flex justify-between text-sm text-gray-500">
-          <span>{publishedDate}</span>
-          <span>{readingTime} read</span>
+    <div className="max-w-sm bg-gray-100 rounded-2xl overflow-hidden">
+      <div className="px-4 mt-4">
+        <img
+          src={imageUrl}
+          alt="heading"
+          className="w-full h-48 object-cover rounded-xl"
+        />
+      </div>
+      <div className="px-4 mt-4">
+        <TopicChips topic={topic} backgroundColor={false} />
+      </div>
+      <div className="px-4 mb-4">
+        <h2 className="text-xl mt-2 font-semibold text-gray-800">{title}</h2>
+        <div className="flex space-x-2 mt-2 items-center font-normal text-sm">
+          <p>{publishedDate}</p>
+          <p>&#8226;</p>
+          <p>{readingTime} min Read</p>
         </div>
       </div>
     </div>
