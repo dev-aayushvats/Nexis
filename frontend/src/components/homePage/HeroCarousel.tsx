8136659b72a943aea77326629428681346ea6946
Satyam Thakur <sat.thakur20@gmail.com>
2024-11-19 01:06:27 +0530

    Adding loader when article date is requested

--- frontend/src/components/homePage/HeroCarousel.tsx
+++ frontend/src/components/homePage/HeroCarousel.tsx
@@ -4,11 +4,13 @@ import { axiosInstance } from '../../config/axios';
 import { CarouselItem } from '../../interfaces/carouselItem';
 import { carouselItems as mockData } from '../../mocks/carouselData';
 import { formatDate } from '../../utils/dateUtils';
+import { DotLoader } from 'react-spinners';
 
 const HeroCarousel: React.FC = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [isAnimating, setIsAnimating] = useState(false);
   const [carouselItems, setCarouselItems] = useState<CarouselItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleNext = () => {
     if (!isAnimating) {
@@ -47,6 +49,8 @@ const HeroCarousel: React.FC = () => {
       } catch (error) {
         console.error('Failed to fetch articles:', error);
         setCarouselItems(mockData); // Set to mock data on error
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -55,104 +59,115 @@ const HeroCarousel: React.FC = () => {
 
   return (
     <div className="container">
-      <div id="carousel" className="relative w-full">
-        {/* Carousel wrapper */}
-        <div className="relative mt-4 h-56 overflow-hidden rounded-xl md:h-[50vh]">
-          {carouselItems.length === 0 ? (
-            <p>No items available</p>
-          ) : (
-            carouselItems.map((item, index) => (
-              <Link
+      {isLoading ? (
+        <div className="flex justify-center items-center h-56">
+          <DotLoader color="#6B4DE6" />
+        </div>
+      ) : (
+        <div id="carousel" className="relative w-full">
+          {/* Carousel wrapper */}
+          <div className="relative mt-4 h-56 overflow-hidden rounded-xl md:h-[50vh]">
+            {carouselItems.length === 0 ? (
+              <p>No items available</p>
+            ) : (
+              carouselItems.map((item, index) => (
+                <Link
+                  key={item._id}
+                  className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${
+                    currentSlide === index
+                      ? 'opacity-100 z-10'
+                      : 'opacity-0 z-0'
+                  }`}
+                  to={`/article/${item._id}`}
+                >
+                  <img
+                    src={item.imageUrl}
+                    alt={item.title}
+                    className="absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                  />
+                  {/* Gradient Overlay */}
+                  <div className="absolute inset-0 bg-gradient-to-t from-gray-800 to-transparent" />
+                  {/* Title, Published Date, and Read Time */}
+                  <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-center text-white m-2">
+                    <h2 className="text-3xl mb-1 font-semibold">
+                      {item.title}
+                    </h2>
+                    <p className="text-sm">
+                      {formatDate(item.postDate)} &bull; {item.readTime} min
+                      read
+                    </p>
+                  </div>
+                </Link>
+              ))
+            )}
+          </div>
+
+          {/* Slider indicators */}
+          <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3">
+            {carouselItems.map((item, index) => (
+              <button
                 key={item._id}
-                className={`absolute inset-0 transition-opacity duration-700 ease-in-out ${
-                  currentSlide === index ? 'opacity-100 z-10' : 'opacity-0 z-0'
+                type="button"
+                className={`w-3 h-3 rounded-full ${
+                  currentSlide === index ? 'bg-primary-300' : 'bg-gray-100'
                 }`}
-                to={`/article/${item._id}`}
+                aria-label={`Slide ${index + 1}`}
+                onClick={() => setCurrentSlide(index)}
+              ></button>
+            ))}
+          </div>
+
+          {/* Previous Button */}
+          <button
+            type="button"
+            className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group"
+            onClick={handlePrev}
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-white/40 group-hover:bg-white/50 dark:group-hover:bg-gray-100/70">
+              <svg
+                className="w-4 h-4 text-white dark:text-primary-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
               >
-                <img
-                  src={item.imageUrl}
-                  alt={item.title}
-                  className="absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 1L1 5l4 4"
                 />
-                {/* Gradient Overlay */}
-                <div className="absolute inset-0 bg-gradient-to-t from-gray-800 to-transparent" />
-                {/* Title, Published Date, and Read Time */}
-                <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-center text-white m-2">
-                  <h2 className="text-3xl mb-1 font-semibold">{item.title}</h2>
-                  <p className="text-sm">
-                    {formatDate(item.postDate)} &bull; {item.readTime} min read
-                  </p>
-                </div>
-              </Link>
-            ))
-          )}
+              </svg>
+              <span className="sr-only">Previous</span>
+            </span>
+          </button>
+
+          {/* Next Button */}
+          <button
+            type="button"
+            className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group"
+            onClick={handleNext}
+          >
+            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-white/40 group-hover:bg-white/50 dark:group-hover:bg-gray-100/70">
+              <svg
+                className="w-4 h-4 text-white dark:text-primary-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 9l4-4-4-4"
+                />
+              </svg>
+              <span className="sr-only">Next</span>
+            </span>
+          </button>
         </div>
-
-        {/* Slider indicators */}
-        <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3">
-          {carouselItems.map((item, index) => (
-            <button
-              key={item._id}
-              type="button"
-              className={`w-3 h-3 rounded-full ${
-                currentSlide === index ? 'bg-primary-300' : 'bg-gray-100'
-              }`}
-              aria-label={`Slide ${index + 1}`}
-              onClick={() => setCurrentSlide(index)}
-            ></button>
-          ))}
-        </div>
-
-        {/* Previous Button */}
-        <button
-          type="button"
-          className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group"
-          onClick={handlePrev}
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-white/40 group-hover:bg-white/50 dark:group-hover:bg-gray-100/70">
-            <svg
-              className="w-4 h-4 text-white dark:text-primary-600"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 1L1 5l4 4"
-              />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-
-        {/* Next Button */}
-        <button
-          type="button"
-          className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group"
-          onClick={handleNext}
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-white/40 group-hover:bg-white/50 dark:group-hover:bg-gray-100/70">
-            <svg
-              className="w-4 h-4 text-white dark:text-primary-600"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 9l4-4-4-4"
-              />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
-      </div>
+      )}
     </div>
   );
 };
