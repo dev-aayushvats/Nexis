2527930e21bf17924699bf7c5dad0c740d3e935a
Satyam Thakur <sat.thakur20@gmail.com>
2024-11-20 01:34:25 +0530

    Fixing ts build errors

--- frontend/src/mocks/carouselData.ts
+++ frontend/src/mocks/carouselData.ts
@@ -1,12 +1,13 @@
-import { CarouselItem } from '../interfaces/articlelItem';
+import { ArticleItem } from '../interfaces/articlelItem';
 
-export const carouselItems: CarouselItem[] = [
+export const carouselItems: ArticleItem[] = [
   {
     _id: '0',
     imageUrl:
       'https://alleninstitute.org/wp-content/uploads/2024/05/Quantumconsciousness2-ezgif.com-crop.jpg',
     title: 'The Quantum Consciousness',
     postDate: 1697068800000,
+    totalLikes: 13,
     readTime: 5,
   },
   {
@@ -15,6 +16,7 @@ export const carouselItems: CarouselItem[] = [
       'https://images.newscientist.com/wp-content/uploads/2019/09/16145926/new-scientist-full.jpg',
     title: 'Exploring the Unknown',
     postDate: 1697356800000,
+    totalLikes: 15,
     readTime: 3,
   },
   {
@@ -23,6 +25,7 @@ export const carouselItems: CarouselItem[] = [
       'https://alleninstitute.org/wp-content/uploads/2024/05/Quantumconsciousness2-ezgif.com-crop.jpg',
     title: 'The Mind-Body Problem',
     postDate: 1697443200000,
+    totalLikes: 16,
     readTime: 2,
   },
   {
@@ -31,6 +34,7 @@ export const carouselItems: CarouselItem[] = [
       'https://images.newscientist.com/wp-content/uploads/2019/09/16145926/new-scientist-full.jpg',
     title: 'Cosmic Mysteries Unveiled',
     postDate: 1697529600000,
+    totalLikes: 18,
     readTime: 6,
   },
   {
@@ -39,6 +43,7 @@ export const carouselItems: CarouselItem[] = [
       'https://alleninstitute.org/wp-content/uploads/2024/05/Quantumconsciousness2-ezgif.com-crop.jpg',
     title: 'Neural Networks and AI',
     postDate: 1697616000000,
+    totalLikes: 33,
     readTime: 8,
   },
 ];
