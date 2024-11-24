048fe870ef91bf90268af3035391688fa24f7c29
Satyam Thakur <sat.thakur20@gmail.com>
2024-11-25 04:53:39 +0530

    Setting up headers in middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,9 +19,22 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // Test to check the protected route using route
 app.get("/api/protected", protect, (req: Request, res: Response) => {
