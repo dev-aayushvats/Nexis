5ba8867eea2d9bc4da95da3149eeaa96f6c85a26
Satyam Thakur <sat.thakur20@gmail.com>
2024-12-01 20:12:55 +0530

    Removing instagram share icon

--- frontend/src/components/articleDetail/ShareArticle.tsx
+++ frontend/src/components/articleDetail/ShareArticle.tsx
@@ -2,33 +2,14 @@ const ShareArticle = ({ postUrl }: { postUrl: string }) => {
   return (
     <div className="bg-primary-400 shadow-lg rounded-xl p-4">
       <h2 className="text-lg text-white font-bold mb-4">Share this article</h2>
-      <div className="flex space-x-4">
+      <div className="flex items-center space-x-4">
         {/* Social Share Links */}
-        <div className="flex flex-col items-center space-y-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="70"
-            height="70"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M 8 3 C 5.239 3 3 5.239 3 8 L 3 16 C 3 18.761 5.239 21 8 21 L 16 21 C 18.761 21 21 18.761 21 16 L 21 8 C 21 5.239 18.761 3 16 3 L 8 3 z M 18 5 C 18.552 5 19 5.448 19 6 C 19 6.552 18.552 7 18 7 C 17.448 7 17 6.552 17 6 C 17 5.448 17.448 5 18 5 z M 12 7 C 14.761 7 17 9.239 17 12 C 17 14.761 14.761 17 12 17 C 9.239 17 7 14.761 7 12 C 7 9.239 9.239 7 12 7 z M 12 9 A 3 3 0 0 0 9 12 A 3 3 0 0 0 12 15 A 3 3 0 0 0 15 12 A 3 3 0 0 0 12 9 z"
-              fill="white"
-            ></path>
-          </svg>
-          <a
-            href={``}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white"
-          >
-            Instagram
-          </a>
-        </div>
-
-        <div className="flex flex-col items-center space-y-1">
+        <a
+          href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(postUrl)}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex flex-col items-center space-y-1"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             x="0px"
@@ -42,17 +23,15 @@ const ShareArticle = ({ postUrl }: { postUrl: string }) => {
               fill="white"
             ></path>
           </svg>
-          <a
-            href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(postUrl)}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white"
-          >
-            Twitter
-          </a>
-        </div>
+          <p className="text-white">Twitter</p>
+        </a>
 
-        <div className="flex flex-col items-center space-y-1">
+        <a
+          href={`https://www.linkedin.com/shareArticle?url=${encodeURIComponent(postUrl)}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex flex-col items-center space-y-1"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             x="0px"
@@ -66,15 +45,8 @@ const ShareArticle = ({ postUrl }: { postUrl: string }) => {
               fill="white"
             ></path>
           </svg>
-          <a
-            href={`https://www.linkedin.com/shareArticle?url=${encodeURIComponent(postUrl)}`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white"
-          >
-            LinkedIn
-          </a>
-        </div>
+          <p className="text-white">LinkedIn</p>
+        </a>
       </div>
     </div>
   );
